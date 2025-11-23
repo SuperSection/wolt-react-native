f3aefa4479095268d66c1628a0ad82faa3e960e5
Soumo Sarkar <soumosarkar297@gmail.com>
2025-11-24 02:26:44 +0530

    feat: Configure `QueryClient` with default `staleTime` and `retry` options for queries.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,7 +3,17 @@ import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 import { useFonts } from "expo-font";
 import { Nunito_400Regular, Nunito_700Bold, Nunito_900Black } from "@expo-google-fonts/nunito";
-const queryClient = new QueryClient();
+
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 5 * 60 * 1000, // 5 minutes
+      retry: 1,
+    }
+  }
+});
+
 
 export default function RootLayout() {
   let [fontsLoaded] = useFonts({
