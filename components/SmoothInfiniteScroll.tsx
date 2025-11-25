81789d3782c43de45b4507486d2fa0a8d61fcb0e
Soumo Sarkar <soumosarkar297@gmail.com>
2025-11-25 18:28:26 +0530

    fix: change the web-only boxShadow styles in `SmoothInfiniteScroll` to preserve visual effect across platforms

--- components/SmoothInfiniteScroll.tsx
+++ components/SmoothInfiniteScroll.tsx
@@ -126,7 +126,11 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         borderRadius: 20,
         marginHorizontal: 5,
-        boxShadow: '0px -2px 10px rgba(0, 0, 0, 0.1)',
+        shadowColor: 'rgba(0, 0, 0, 0.1)',
+        shadowOffset: { width: 0, height: -2 },
+        shadowOpacity: 0.1,
+        shadowRadius: 10,
+        elevation: 2,
     },
     iconText: {
         fontSize: 50,
